0d65ff5c56aab1561260ada7b41514fe84033886
Mavlarn <mavlarn@liepiaowang.com>
2016-09-06 16:28:39 +0800

    Update some style and name.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'test-app',
+    selector: 'root-app',
     templateUrl: 'app/app.component.html',
     styleUrls: ['app/app.component.css']
 })
